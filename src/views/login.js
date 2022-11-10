b01aeb60369d5581fc754d94db11aaf5e1235392
Matheus Ned <matheusned15@gmail.com>
2022-11-09 23:06:34 -0300

    Componentizando o formulário de Login

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
+import FormGroup from "../components/form-group";
 
 class Login extends React.Component {
 
@@ -10,7 +11,27 @@ class Login extends React.Component {
                     <div className="col-md-6" style={{position: "relative", left: '300px'}}>
                         <div className="bs-docs-section">
                             <Card title="Login">
-                                Meu cartão
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="bs-component">
+                                            <fieldset>
+                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail ">
+                                                    <input type="email"
+                                                           className="form-group"
+                                                           id="exampleInputEmail"
+                                                           aria-describedby="emailHelp"
+                                                           placeholder="Digite o Email"/>
+                                                </FormGroup>
+                                                <FormGroup label="Senha: *" htmlFor="exampleInputEmail">
+                                                    <input type="password"
+                                                           className="form-group"
+                                                           id="exampleInputPassword1"
+                                                           placeholder="Password"/>
+                                                </FormGroup>
+                                            </fieldset>
+                                        </div>
+                                    </div>
+                                </div>
                             </Card>
 
                         </div>
